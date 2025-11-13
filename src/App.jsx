d2ccf128c92785c0ed6279bb5285d15461de96ae
SaraFaizi557 <sarafaizi2010@gmail.com>
2025-11-13 21:10:41 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder='Enter notes heading' />
+      </form>
+    </div>
+  )
+}
+
+export default App
